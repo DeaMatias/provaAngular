676b994f055537d32e3fab85a5b739ce495a0240
Andrea <dea.cmatias@gmail.com>
2021-02-10 16:53:43 -0300

    Avaliação Angular

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,38 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { BlocksComponent } from './pages/blocks/blocks.component';
+import { CardComponent } from './pages/card/card.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { FormsComponent } from './pages/forms/forms.component';
+import { PricingComponent } from './pages/pricing/pricing.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: DashboardComponent
+},
+{
+  path: 'card',
+  component: CardComponent
+},
+
+{
+  path: 'pricing',
+  component: PricingComponent
+},
+
+{
+  path: 'blocks',
+  component: BlocksComponent
+},
+
+{
+  path: 'forms',
+  component: FormsComponent
+}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,32 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ComponentComponent } from './component/component.component';
+import { HeaderComponent } from './component/header/header.component';
+import { SidebarComponent } from './component/sidebar/sidebar.component';
+import { PagesComponent } from './pages/pages.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { CardComponent } from './pages/card/card.component';
+import { PricingComponent } from './pages/pricing/pricing.component';
+import { BlocksComponent } from './pages/blocks/blocks.component';
+import { FormsComponent } from './pages/forms/forms.component';
+import { FooterComponent } from './component/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ComponentComponent,
+    HeaderComponent,
+    SidebarComponent,
+    PagesComponent,
+    DashboardComponent,
+    CardComponent,
+    PricingComponent,
+    BlocksComponent,
+    FormsComponent,
+    FooterComponent
   ],
+
   imports: [
     BrowserModule,
     AppRoutingModule
@@ -15,4 +36,5 @@ import { AppComponent } from './app.component';
   providers: [],
   bootstrap: [AppComponent]
 })
+
 export class AppModule { }
